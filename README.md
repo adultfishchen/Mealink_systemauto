776b5ff3a8097cc87e39b380bfb88d4e83fd5d9d
linda <adultfish17640336@gmail.com>
2020-05-09 19:31:55 +0800

    try pushh all files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mealink Systemauto
+Graduated Project!
---
+++ auto/app.js
@@ -0,0 +1,300 @@
+require("dotenv").config();
+
+var express = require("express");
+var app = express();
+var MongoClient = require("mongodb").MongoClient;
+var similarity = require("compute-cosine-similarity");
+var schedule = require("node-schedule");
+
+var url = "mongodb://localhost:27017/";
+
+
+function scheduleClearCronstyle(){
+  schedule.scheduleJob("00 43 18 * * *", function(){
+    MongoClient.connect(
+      url,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      function (err, db) {
+        if (err) {
+          console.log("hello");
+          throw err;
+        }
+        var dbo = db.db("mealinktesting_project");
+        var users = dbo
+          .collection("users")
+          .find()
+          .toArray(function (err, result) {
+            if (err) {
+              console.log("hello");
+              throw err;
+            }
+            // console.log(result);
+            // console.log(result[0]);
+            for (z = 0; z < result.length; z++) {
+              dbo
+                .collection("users")
+                .updateOne({ _id: result[z]._id }, { $set: { match: null } });
+            }
+                console.log("delete!!");
+              
+            }
+                )}
+    );
+      console.log("scheduleClearCronstyle:" + new Date());
+  }); 
+}
+
+
+function scheduleCronstyle(){
+  schedule.scheduleJob("00 44 18 * * *", function(){
+    MongoClient.connect(
+      url,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      function (err, db) {
+        if (err) {
+          console.log("hello");
+          throw err;
+        }
+        var userdouble = [];
+        var manyusers= [];
+        var dbo = db.db("mealinktesting_project");
+        var users = dbo
+          .collection("users")
+          .find()
+          .toArray(function (err, result) {
+            if (err) {
+              console.log("hello");
+              throw err;
+            }
+            // console.log("result :" + result);
+            // console.log(result[0]);
+            for(a= 0 ; a< result.length; a++){
+              if(result[a].number ===2){
+                userdouble.push(result[a]);
+                for (b = 0; b < userdouble.length; b++) {
+                  if (userdouble[b].match === null) {
+                    var baseuser = userdouble[b];
+                    var basevalue = [];
+                    for (c = 0; c < userdouble[b].matchHabies.select.length; c++) {
+                      var value = userdouble[b].matchHabies.select[c] * 1;
+                      // console.log(value);
+                      basevalue.push(value);
+                      var e = []; // similarity
+                      var d = []; // compuserid
+                    }
+    
+                    for (f = b + 1; f < userdouble.length; f++) {
+                      var compuser = null;
+                      if (userdouble[f].match === null) {
+                        compuser = userdouble[f];
+                        var compvalue = [];
+                        for (g = 0; g < userdouble[f].matchHabies.select.length; g++) {
+                          var comvalue = userdouble[f].matchHabies.select[g] * 1;
+                          compvalue.push(comvalue);
+                        }
+        
+                        // console.log(compvalue);
+                        var s2 = similarity(basevalue, compvalue);
+                        if (isNaN(s2)) {
+                          s2 = 0;
+                        }
+                        // console.log("rs:"+s);
+                        d.push(userdouble[f]);
+                        // console.log(d);
+                        if(s2>0){
+                          e.push(s2);
+                        console.log("re:" + e);
+                      }
+                        
+                      }
+    
+                    }
+                    var ans = Math.max(...e);
+                    for (h = 0; h < e.length; h++) {
+                      if (e[h] === ans) {
+                        compuser = d[h];
+
+                           dbo
+                          .collection("users")
+                          .updateOne(
+                            { _id: baseuser._id },
+                            { $set: { match: compuser._id} }
+                          );
+                        dbo
+                          .collection("users")
+                          .updateOne(
+                            { _id: compuser._id },
+                            { $set: { match: baseuser._id } }
+                          );  
+                          break;
+
+                      }
+                    }
+                    
+                    console.log("successful!!");
+                  }
+                  
+                }               
+              } else {
+                manyusers.push(result[a]);               
+              }
+             }
+           
+            
+            //manyusers
+            for (i = 0; i < manyusers.length; i++) {
+              if (manyusers[i].match === null) {
+                var mbaseuser = manyusers[i];
+                // console.log(mbaseuser);
+                var mbasevalue = [];
+                for (j = 0; j < manyusers[i].matchHabies.select.length; j++) {
+                  var mvalue = manyusers[i].matchHabies.select[j] * 1;
+                  // console.log(mvalue);
+                  mbasevalue.push(mvalue);
+                  var n = []; // similarity
+                  var m= []; // compuserid
+                }
+
+                for (k = i + 1; k < manyusers.length; k++) {
+                  var mcompuser = null;
+                  if (manyusers[k].match === null) {
+                    mcompuser = manyusers[k];
+                    // console.log( "mcompuser: " + manyusers[k]._id);
+                    var mcompvalue = [];
+                    for (l = 0; l < manyusers[k].matchHabies.select.length; l++) {
+                      var mcomvalue = manyusers[k].matchHabies.select[l] * 1;
+                      // console.log(mcomvalue);
+                      mcompvalue.push(mcomvalue);
+                    }        
+                    
+                   
+                     var s3 = similarity(mbasevalue, mcompvalue);
+                      if (isNaN(s3)) {
+                       s3 = 0;
+                     }
+                     m.push(manyusers[k]._id);
+                    //  console.log("allcompusers: " + m);
+                     n.push(s3);
+                    //  console.log("re:" + s3);
+                  }
+
+                }
+                var mans = [];                
+                for(o=0 ; o< n.length; o++){
+                  mans.push(n[o]);
+                }
+                mans.sort(function(a, b){return b-a});
+                // console.log("mans:" + mans);
+
+                var mcusermatches=[];
+                for(p=0; p< mans.length; p++){                    
+                      if(mans[p]>0){
+                        for(q=0;q<mans.length;q++){
+                          if(mans[p]=== n[q]){
+                            mcompuser = m[q];                       
+                        mcusermatches.push(m[q]); 
+                        // console.log("mcumusers:" + mcusermatches)
+                          }                     
+                        } 
+                        dbo
+                    .collection("users")
+                    .updateOne(
+                      { _id: mbaseuser._id },
+                      { $set: { match: [mcusermatches[0],mcusermatches[1],mcusermatches[2]]} }
+                    );
+                  dbo
+                    .collection("users")
+                    .updateOne(
+                      { _id: mcusermatches[0] },
+                      { $set: { match: [ mbaseuser._id, mcusermatches[1], mcusermatches[2]]}}
+                    );
+                    dbo
+                    .collection("users")
+                    .updateOne(
+                      { _id: mcusermatches[1] },
+                      { $set: { match: [ mbaseuser._id, mcusermatches[0], mcusermatches[2]] }}
+                    );
+                    dbo
+                    .collection("users")
+                    .updateOne(
+                      { _id: mcusermatches[2] },
+                      { $set: { match: [ mbaseuser._id, mcusermatches[0], mcusermatches[1]]}}
+                    );
+                   }
+                        
+                }              
+                break;
+                }             
+
+                console.log("successful!!");
+              }
+
+           });
+      }
+    );
+      console.log("scheduleDoubleCronstyle:" + new Date());
+  }); 
+}
+
+function scheduleFriendCronstyle(){
+  schedule.scheduleJob("00 45 18 * * *", function(){
+    MongoClient.connect(
+      url,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      function (err, db) {
+        if (err) {
+          console.log("hello");
+          throw err;
+        }
+        var dbo = db.db("mealinktesting_project");
+        var users = dbo
+          .collection("users")
+          .find()
+          .toArray(function (err, result) {
+            if (err) {
+              console.log("hello");
+              throw err;
+            }
+            // console.log(result);
+            // console.log(result[0]);
+            for (y = 0; y < result.length; y++) {
+              if(result[y].friends === null && result[y].match!=null){
+                dbo
+                .collection("users")
+                .updateOne({ _id: result[y]._id }, { $set: { friends: result[y].match } });
+            } else {
+              dbo
+                .collection("users")
+                .updateOne({ _id: result[y]._id }, { $addToSet: { "friends":{$each:[result[y].match ]}}} );
+            }
+              }
+              
+                console.log("Friend Add!!");
+              
+            }
+                )}
+    );
+      console.log("scheduleClearCronstyle:" + new Date());
+  }); 
+}
+
+scheduleClearCronstyle();
+scheduleCronstyle();
+scheduleFriendCronstyle();
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
